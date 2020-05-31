4b75d64c42a27c3e9cd8f268528c8c13ce85e069
Claire <claire.cy.t@gmail.com>
2020-05-31 01:40:43 -0400

    Use Json token to authenticate user and retrieve user info

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const mongoConfig = require('./constants/mongoConfig');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -23,8 +24,8 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err);
 });
 
-app.get('/', (req, res) =>{
-    res.send('Hi there!');
+app.get('/', requireAuth, (req, res) =>{
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000,() =>{
